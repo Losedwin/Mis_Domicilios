d5553bba2045e2de550dbad086eda5acae6dc598
Losedwin <losedwins55@gmail.com>
2026-09-13 08:59:17 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "domi-cache-v1";
+const CACHE_NAME = "domi-cache-v2";
 const FILES = ["./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
